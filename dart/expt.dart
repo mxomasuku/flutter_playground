ab6441aa34c885e4a57bed6f3755b9dba0c4f07b
Mxolisi Masuku <mxolisi.b.masuku@gmail.com>
2025-02-13 22:21:28 +0200

    Save New Folder Structure

--- dart/expt.dart
+++ dart/expt.dart
@@ -1,48 +1,30 @@
+import '../dart/user.dart';
+
 void main() {
-  @override
-  const String hello = "hello";
+  var humanBeing = User("humanBeing", 78);
+  humanBeing.describe();
 
-//changable types
-  dynamic gender = "female";
+  var happyPerson = User.unborn("Unborn");
+  happyPerson.describe();
 
-  gender = greeting();
-  int age = getAge();
-  User userOne = User("Mario", 390);
+  print("Hello World");
 
-  //LIST TYPE IN DART: it works just like an array, and you can add generic types like in typescript
-  List<String> hobbies = ["chess", "gym", "boxing"];
+  var name = "hello there";
+  print(name);
+  var randomNumber = fibonacci(33);
+  print(randomNumber);
 
-  hobbies.add("sex");
-
-  print(hello);
-  print(gender);
-  print(age);
-  print(hobbies);
-  print(userOne.age);
-  userOne.isLoggedIn();
+  flybyObjects.where((name) => name.contains("turn")).forEach(print);
 }
 
-String greeting() {
-  const String salutation = "Mr Masuku Sir";
-
-  return salutation;
-}
-
-//one line arrow function
-int getAge() => 30;
-
-//CLASSES
-class User {
- late String userName;
- late int age;
-
-//how to initialize a constructor function
-  User(String userName, int age) {
-    this.userName = userName;
-    this.age = age;
-  }
-
-  void isLoggedIn() {
-    print("is logged in");
+int fibonacci(int hallo) {
+  if (hallo > 12 || hallo == 6) {
+    const fibValue = 200;
+    return fibValue;
+  } else {
+    const fibValue = 50;
+    return fibValue;
   }
 }
+
+const List<String> flybyObjects = ["Jupiter", "Saturn", "Neptune"];
--- dart/user.dart
+++ dart/user.dart
@@ -1,4 +1,28 @@
 class User {
-  String user = "Masuku";
-  int age = 30;
+  late String userName;
+  late dynamic age;
+  late DateTime birthday;
+
+   int? get birthYear => birthday.year;
+
+  User(String userName, dynamic age) {
+    this.userName = userName;
+    this.age = age;
+  }
+
+  User.unborn(String userName) : this(userName, null);
+  void isLoggedIn() {
+    print("is logged in");
+  }
+
+  void describe() {
+    print("Person: $userName");
+    var birthday = this.birthday;
+    if (birthday != null) {
+      int years = DateTime.now().difference(birthday).inDays ~/ 365;
+      print("Was born: $birthYear ($years years ago)");
+    } else {
+      print("Unborn");
+    }
+  }
 }
