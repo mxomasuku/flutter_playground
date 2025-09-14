82a9b80cbfbf098dfb6f487254c35bf8e5330e9d
Mxolisi Masuku <mxolisi.b.masuku@gmail.com>
2025-09-14 20:00:43 +0200

    feat: meals app category screen

---
+++ meals/lib/screens/categories.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  final List<Category> usableCategories = availableCategories;
+
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      appBar: AppBar(title: const Text("Pick Your Category")),
+      body: GridView(
+        padding: EdgeInsets.all(4),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          // for (final category in usableCategories)
+          //   CategoryGridItem(category: category),
+          ...usableCategories.map(
+            (category) => CategoryGridItem(category: category),
+          ),
+        ],
+      ),
+    ));
+  }
+}
