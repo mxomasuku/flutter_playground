f22255ba6b53a2a3f7339ae976c2f7280be76d13
Mxolisi Masuku <mxolisi.b.masuku@gmail.com>
2025-09-24 13:09:26 +0200

    chore: add shopping list app

---
+++ shopping_list/lib/screens/main_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainList extends StatelessWidget {
+  const MainList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Shopping List")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
