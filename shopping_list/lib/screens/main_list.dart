389db0cd3f23c19e542c9f63c8b1844342edf532
Mxolisi Masuku <mxolisi.b.masuku@gmail.com>
2025-09-26 02:48:34 +0200

    feat: add shopping item list

--- shopping_list/lib/screens/main_list.dart
+++ shopping_list/lib/screens/main_list.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 
 class MainList extends StatelessWidget {
   const MainList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final availableGroceryItems = groceryItems;
+
     return Scaffold(
       appBar: AppBar(title: const Text("Shopping List")),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              
-            )
-          ],
+      body: ListView.builder(
+        itemCount: availableGroceryItems.length,
+
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(availableGroceryItems[index].name),
+          leading: Container(
+            height: 24,
+            width: 24,
+            color: availableGroceryItems[index].category.color,
+          ),
+          trailing: Text(availableGroceryItems[index].quantity.toString()),
         ),
       ),
     );
